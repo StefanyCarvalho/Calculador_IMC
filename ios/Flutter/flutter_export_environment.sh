41eab5a65f15b45fb8a95e14d9a03711a6246f6a
Stefany Carvalho <jmenezes241@gmail.com>
2019-09-14 14:50:57 -0300

    Updates

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Familia\Desktop\estudos\IMC\Calculador_IMC"
+export "FLUTTER_APPLICATION_PATH=C:\Stefany\Calculador_IMC"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
